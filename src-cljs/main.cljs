77595b6f8e093633a10fac1cd29a0fa9dd7c1779
Scott Feeney <scott@oceanbase.org>
2013-03-06 14:59:26 -0500

    Draw hex tops

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -9,12 +9,32 @@
     (apply canvas/line-to ctx pt))
   (canvas/stroke ctx))
 
+(defn vectors->path [x0 y0 vs]
+  "Convert a series of relative vectors to a path starting at x0, y0."
+  (loop [x x0, y y0, acc [], vs vs]
+    (if (empty? vs)
+      (conj acc [x y])
+      (let [dx ((first vs) 0)
+            dy ((first vs) 1)]
+        (recur (+ x dx) (+ y dy) (conj acc [x y]) (rest vs))))))
+
+(defn get-zigzag-pts [x0 y0 dx dy n]
+  "Generate vector of points for n vertically-zigzagging lines."
+  (->>
+    (cycle [[dx dy] [dx (- dy)]])
+    (take n)
+    (vec)
+    (vectors->path x0 y0)))
+
 (defn ^:export draw [board]
   (let [w 700 h 500 cols 10 rows 10
         geom (hexboard-geometry w h cols rows)
         ctx (canvas/get-context board "2d")]
-    (draw-path ctx
-               [[150.5 150.5]
-                [200.5 150.5]
-                [175.5 183.5]
-                [150.5 150.5]])))
+    (dotimes [row (+ 1 rows)]
+      (let [row-top (* row (:row-h geom))
+            row-indent (+ (* (- row 1) (:diag-x geom))
+                          (* (.-lineWidth ctx) 0.5))]
+        (draw-path ctx
+                   (get-zigzag-pts row-indent row-top
+                                   (:diag-x geom) (:diag-y geom)
+                                   (* 2 cols)))))))
