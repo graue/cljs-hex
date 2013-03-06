b953139c731c27e9bd1f3ec2faa4f7dcacf05422
Scott Feeney <scott@oceanbase.org>
2013-03-06 11:44:10 -0500

    Generic draw-path function

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -2,14 +2,19 @@
   (:require [monet.canvas :as canvas])
   (:use     [hexcanvas.geometry :only (hexboard-geometry)]))
 
+(defn draw-path [ctx pts]
+  (canvas/begin-path ctx)
+  (apply canvas/move-to ctx (first pts))
+  (doseq [pt (rest pts)]
+    (apply canvas/line-to ctx pt))
+  (canvas/stroke ctx))
+
 (defn ^:export draw [board]
   (let [w 700 h 500 cols 10 rows 10
-        geom (hexboard-geometry w h cols rows)]
-    (->
-      (canvas/get-context board "2d")
-      (canvas/begin-path)
-      (canvas/move-to 150.5 150.5)
-      (canvas/line-to 200.5 150.5)
-      (canvas/line-to 175.5 183.5)
-      (canvas/line-to 150.5 150.5)
-      (canvas/stroke))))
+        geom (hexboard-geometry w h cols rows)
+        ctx (canvas/get-context board "2d")]
+    (draw-path ctx
+               [[150.5 150.5]
+                [200.5 150.5]
+                [175.5 183.5]
+                [150.5 150.5]])))
