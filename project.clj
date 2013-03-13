a590f7a3567301a070d6a9692c1422ca813e3738
Scott Feeney <scott@oceanbase.org>
2013-03-13 16:14:09 -0400

    Update Monet to 0.1.6-SNAPSHOT
    Sadly it doesn't fix the non-working circle, clear-rect and fill-style
    functions.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "MIT License"
             :url "https://github.com/graue/luasynth/blob/master/MIT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [rm-hull/monet "0.1.4-SNAPSHOT"]]
+                 [rm-hull/monet "0.1.6-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "0.3.0"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds
